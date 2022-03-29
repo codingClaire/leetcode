db1fc3af7a5b744551c92f867d77b39cdbc4883e
codingClaire <suyueyinyin@126.com>
2022-03-29 22:38:18 +0800

    finish 122

--- totalProblem(CN)/26.删除有序数组中的重复项.cpp
+++ totalProblem(CN)/26.删除有序数组中的重复项.cpp
@@ -5,19 +5,23 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int removeDuplicates(vector<int>& nums) {
-        int len=nums.size();
+    int removeDuplicates(vector<int> &nums)
+    {
+        int len = nums.size();
         //双指针i,j
-        int j=0;
-        for (int i=1; i<len;i++){
-            if(nums[i]!=nums[i-1]){
+        int j = 0;
+        for (int i = 1; i < len; i++)
+        {
+            if (nums[i] != nums[i - 1])
+            {
                 j++;
                 nums[j] = nums[i];
             }
         }
-        return j+1;
+        return j + 1;
     }
 };
 // @lc code=end
